dcdf5be871c86aa14dab2f92a93e60245c6fa6e8
duaashehada <60273720+duaashehada@users.noreply.github.com>
2021-11-05 17:46:14 +0300

    add three end pointes that send requests to the catalog and order servers and display the response again , used the ip address of the network in the http request

--- Bazar/index.js
+++ Bazar/index.js
@@ -2,16 +2,19 @@ const { request } = require("express");
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
+const bodyParser = require("body-parser");
 // const fetch = require("node-fetch");
 
 const app = express();
 
 app.use(router);
 
+router.use(bodyParser.json());
+
 router.get("/search", async (req, res) => {
   const Topic = req?.query?.Topic;
   let result;
-  axios.get(`http://localhost:3000/search?Topic=${Topic}`).then((resp) => {
+  axios.get(`http://192.168.1.17:3000/search?Topic=${Topic}`).then((resp) => {
     // console.log(resp.data);
     result = resp.data;
     console.log(result);
@@ -22,7 +25,7 @@ router.get("/search", async (req, res) => {
 router.get("/info", async (req, res) => {
   const id = req?.query?.id;
   let result;
-  axios.get(`http://localhost:3000/info?id=${id}`).then((resp) => {
+  axios.get(`http://192.168.1.17:3000/info?id=${id}`).then((resp) => {
     // console.log(resp.data);
     result = resp.data;
     console.log(result);
@@ -31,10 +34,9 @@ router.get("/info", async (req, res) => {
 });
 
 router.post("/purchase", async (req, res) => {
-  const id = req?.query?.id;
+  const id = req?.body?.id;
   let result;
-  axios.post(`http://localhost:5000/purchase?id=${id}`).then((resp) => {
-    // console.log(resp.data);
+  axios.post(`http://192.168.1.17:5000/purchase`, { id: id }).then((resp) => {
     result = resp.data;
     console.log(result);
     res.json(result);
