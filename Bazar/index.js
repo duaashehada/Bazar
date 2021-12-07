86fefc54ed6936e4abb9af85a0d9c512f69da526
duaashehada <60273720+duaashehada@users.noreply.github.com>
2021-12-07 19:42:48 +0300

    add middleware to the end-points and add the parameter and the response data to cache on the first call to the server

--- Bazar/index.js
+++ Bazar/index.js
@@ -32,36 +32,39 @@ app.use(router);
 
 router.use(bodyParser.json());
 
-router.get("/search", async (req, res) => {
+router.get("/search", verifyCache, async (req, res) => {
   const Topic = req?.query?.Topic;
   let result;
   console.log("request from bazat to catalog ");
   axios.get(`http://10.5.0.5:3000/search?Topic=${Topic}`).then((resp) => {
     // console.log(resp.data);
     result = resp.data;
+    cache.set(Topic, result);
     console.log(result);
     res.json(result);
   });
 });
 
-router.get("/info", async (req, res) => {
+router.get("/info", verifyCache, async (req, res) => {
   const id = req?.query?.id;
   let result;
   console.log("request from bazat to catalog ");
   axios.get(`http://10.5.0.5:3000/info?id=${id}`).then((resp) => {
     // console.log(resp.data);
     result = resp.data;
+    cache.set(id, result);
     console.log(result);
     res.json(result);
   });
 });
 
-router.post("/purchase", async (req, res) => {
-  const id = req?.body?.id;
+router.post("/purchase", verifyCache, async (req, res) => {
+  const id_post = req?.body?.id;
   let result;
 
-  axios.post(`http://10.5.0.6:5000/purchase`, { id: id }).then((resp) => {
+  axios.post(`http://10.5.0.6:5000/purchase`, { id: id_post }).then((resp) => {
     result = resp.data;
+    cache.set(id_post, result);
     console.log(result);
     console.log("request from bazar to order");
     res.json(result);
