db73aee308ab7d001b3932681a8486c061fe3cc7
duaashehada <60273720+duaashehada@users.noreply.github.com>
2021-12-08 22:41:53 +0300

    replicate catalog and order servers

---
+++ catalog-server2/index.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
+
+const app = express();
+app.use(router);
+
+router.get("/", (req, res) => {
+  res.json("hello world from catalog");
+});
+
+router.get("/search", (req, res) => {
+  let Topic = req?.query?.Topic;
+  const result = [];
+  console.log("in catalog in search end point");
+  let fileData = fs.readFileSync("./BazarBooks.json");
+  let BazarBooks = JSON.parse(fileData);
+  for (const element of BazarBooks) {
+    if (element["topic"] == Topic) {
+      result.push(element);
+    }
+  }
+
+  res.json(result);
+});
+
+router.get("/info", (req, res) => {
+  const temp = req?.query?.id;
+  let id = parseInt(temp);
+  console.log("in catalog in info end point");
+  let fileData = fs.readFileSync("./BazarBooks.json");
+  let BazarBooks = JSON.parse(fileData);
+  const result = [];
+  for (const element of BazarBooks) {
+    if (element["id"] == id) {
+      result.push(element);
+    }
+  }
+  res.json(result);
+});
+
+router.put("/update", (req, res) => {
+  const temp = req?.query?.id;
+  let id = parseInt(temp);
+  console.log("in catalog in update end point");
+  let fileData = fs.readFileSync("./BazarBooks.json");
+  let BazarBooks = JSON.parse(fileData);
+  const result = [];
+  for (const element of BazarBooks) {
+    if (element["id"] == id) {
+      element["number of item in stock"] =
+        parseInt(element["number of item in stock"]) - 1;
+      result.push(element);
+    }
+  }
+  // result[0]["number of item in stock"] =
+  //   parseInt(result[0]["number of item in stock"]) - 1;
+
+  fs.writeFile("BazarBooks.json", JSON.stringify(BazarBooks), (error) => {
+    if (error != null) {
+      console.log(error);
+    }
+  });
+
+  res.json(result);
+});
+
+const PORT = process.env.PORT || 7000;
+app.listen(PORT, "0.0.0.0", console.log(`server is running on ${PORT}`));
---
+++ order-server2/index.js
@@ -0,0 +1,44 @@
+const { request } = require("express");
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+
+const app = express();
+
+app.use(router);
+
+router.use(bodyParser.json());
+
+router.post("/purchase", (req, res) => {
+  const id = req?.body?.id;
+  let result;
+  console.log("in order in purchase end point");
+  let orderFileData = fs.readFileSync("./OrderList.json");
+  let OrderList = JSON.parse(orderFileData);
+  axios.get(`http://10.5.0.5:3000/info?id=${id}`).then((resp) => {
+    // console.log(resp.data);
+    result = resp.data;
+    if (parseInt(result[0]["number of item in stock"]) > 0) {
+      axios.put(`http://10.5.0.5:3000/update?id=${id}`).then((e) => {
+        finalResult = e.data;
+        console.log(finalResult);
+        OrderList.push(finalResult[0]);
+        fs.writeFile("OrderList.json", JSON.stringify(OrderList), (error) => {
+          if (error != null) {
+            console.log(error);
+          }
+        });
+        res.json(`you purchaced the ${finalResult[0]["titel"]} successfuly :)`);
+      });
+    } else {
+      res.json(`this item not found :(`);
+    }
+  });
+});
+
+const PORT = process.env.PORT || 6000;
+app.listen(PORT, "0.0.0.0", console.log(`server is running on ${PORT}`));
+
+module.exports = router;
